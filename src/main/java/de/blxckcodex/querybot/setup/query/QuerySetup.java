ba589b6ea549aa375e552cac4f23a603647ddc91
RealBlxckCodex <concrafter78@gmail.com>
2022-02-14 20:32:29 +0100

    Added Setup Class

--- src/main/java/de/blxckcodex/querybot/setup/query/QuerySetup.java
+++ src/main/java/de/blxckcodex/querybot/setup/query/QuerySetup.java
@@ -1,4 +1,29 @@
 package de.blxckcodex.querybot.setup.query;
 
+import de.blxckcodex.querybot.Main;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+@Getter
+@Setter
 public class QuerySetup {
+
+
+
+    public boolean isSetupDone(){
+        Path path = Paths.get("QueryConfig.json");
+        return path.toFile().isFile();
+    }
+
+    public void start(){
+        Scanner scanner = new Scanner(System.in);
+        Main.ClearConsole();
+
+    }
+
+
 }
