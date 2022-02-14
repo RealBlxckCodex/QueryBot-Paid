13187fb3c4e0352c947c2e4eb43277792c1aa79c
RealBlxckCodex <concrafter78@gmail.com>
2022-02-14 20:35:13 +0100

    Added some new methods

--- src/main/java/de/blxckcodex/querybot/Main.java
+++ src/main/java/de/blxckcodex/querybot/Main.java
@@ -3,14 +3,42 @@ package de.blxckcodex.querybot;
 import de.blxckcodex.querybot.bot.QueryBot;
 import de.blxckcodex.querybot.bot.QueryConnectionData;
 import de.blxckcodex.querybot.message.BotData;
+import de.blxckcodex.querybot.setup.query.QuerySetup;
 
 public class Main {
 
     public static void main(String[] args){
-        QueryConnectionData queryConnectionData = new QueryConnectionData("localhost", "serveradmin", "QueryBot", "o89ncq5E", 10011, 1, 9987);
-        BotData botData = new BotData("Test", "Es wurden $(currentteam) Teamler benachrichtigt.", "Es ist gerade $(clientname) im Support.", "", "", "", "", "", "", "", "", 11, 17, 3,1,13,3);
-        QueryBot queryBot = new QueryBot(queryConnectionData, botData);
-        queryBot.connect();
+
+        QuerySetup querySetup = new QuerySetup();
+
+        if(querySetup.isSetupDone() == true){
+            QueryConnectionData queryConnectionData = new QueryConnectionData("localhost", "serveradmin", "QueryBot", "o89ncq5E", 10011, 1, 9987);
+            BotData botData = new BotData("Test", "Es wurden $(currentteam) Teamler benachrichtigt.", "Es ist gerade $(clientname) im Support.", "", "", "", "", "", "", "", "", 11, 17, 3,1,13,3);
+            QueryBot queryBot = new QueryBot(queryConnectionData, botData);
+            queryBot.connect();
+        } else {
+
+        }
+
     }
+    public static void ClearConsole(){
+        try{
+            String operatingSystem = System.getProperty("os.name"); //Check the current operating system
+
+            if(operatingSystem.contains("Windows")){
+                ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "cls");
+                Process startProcess = pb.inheritIO().start();
+                startProcess.waitFor();
+            } else {
+                ProcessBuilder pb = new ProcessBuilder("clear");
+                Process startProcess = pb.inheritIO().start();
+
+                startProcess.waitFor();
+            }
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
 
 }
---
+++ src/main/java/de/blxckcodex/querybot/QueryManager.java
@@ -0,0 +1,12 @@
+package de.blxckcodex.querybot;
+
+public class QueryManager {
+
+    public void startQuery(){
+    }
+
+    private void getConfigurationProperties(){
+
+    }
+
+}
